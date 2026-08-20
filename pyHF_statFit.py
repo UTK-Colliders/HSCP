9b29b2dd13b663bc7a27d6bf840dfb497e7fb358
ctdax <colbythomps@gmail.com>
2026-08-20 19:08:51 +0200

    Added parameter initalization and expected_actualdata

--- pyHF_statFit.py
+++ pyHF_statFit.py
@@ -1,4 +1,4 @@
-# First run `source pyHF/bin/activate` to access the pyhf library
+# First run `/usr/bin/python3.12 -m venv pyHF`, followed by `source pyHF/bin/activate` to access the pyhf library
 # For your first time, run `pip install pyhf`
 # Run `deactivate` when you are done with the venv
 
@@ -23,17 +23,27 @@ def debug_print(model):
     print(f" modifiers: {model.config.modifiers}")
     print(f"parameters: {model.config.parameters}")
     print(f"  nauxdata: {model.config.nauxdata}")
-    print(f"   auxdata: {model.config.auxdata}")
+    print(f"   auxdata: {model.config.auxdata}\n")
+
+    print(f"Suggested initial parameters:          {model.config.suggested_init()}")
+    print(f"Suggested parameter bounds:            {model.config.suggested_bounds()}")
+    print(f"Should parameters be fixed during fit: {model.config.suggested_fixed()}")
 
 def main():
     parser = argparse.ArgumentParser(description="Perform a single-channel statistical fit with uncorrelated background")
-    parser.add_argument("--n_signal", default=1.0)
-    parser.add_argument("--n_background", default=1.0)
-    parser.add_argument("--background_uncertainty", default=1.0)
-    parser.add_argument("--do_debug_print", default=False)
+    parser.add_argument("--n_signal", type=float, default=1.0)
+    parser.add_argument("--n_background", type=float, default=1.0)
+    parser.add_argument("--background_uncertainty", type=float, default=1.0)
+    parser.add_argument("--do_debug_print", type=bool, default=True)
     args = parser.parse_args()
 
     model = build_model(args.n_signal, args.n_background, args.background_uncertainty, args.do_debug_print)
+
+    # For a single channel, the parameters are [mu, gamma_b] (see https://pyhf.github.io/pyhf-tutorial/helloworld/)
+    # Mu is the signal strength, gamma_b is a multiplicative factor calculated by the background uncertainty given an uncorrelated background
+    # Here by default the model automatically calculates what mu and gamma_b should be
+    init_pars = model.config.suggested_init()
+    model.expected_actualdata(init_pars)
 
 if __name__ == "__main__":
     main()
