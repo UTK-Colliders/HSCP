fb03034a50eaee4e2982071c17734182c224bf3e
ctdax <colbythomps@gmail.com>
2026-08-20 19:37:50 +0200

    Added log loglikelihood

--- pyHF_statFit.py
+++ pyHF_statFit.py
@@ -6,7 +6,7 @@ import pyhf
 import argparse
 
 
-def build_model(n_signal, n_background, background_uncertainty, do_debug_print):
+def build_model(n_signal, n_background, background_uncertainty):
     model = pyhf.simplemodels.uncorrelated_background(
         signal=[n_signal], bkg=[n_background], bkg_uncertainty=[background_uncertainty]
     )
@@ -15,6 +15,19 @@ def build_model(n_signal, n_background, background_uncertainty, do_debug_print):
         debug_print(model)
 
     return model
+
+def background_only_parameters(model, pars):
+    bkg_pars = pars.copy()
+    bkg_pars[model.config.poi_index] = 0
+    return bkg_pars
+
+def compute_log_likelihood(model, n_observed, sigPlusBkg_pars, bkg_pars):
+    observations = [n_observed] + model.config.auxdata
+    if do_debug_print:
+        print(f"Log likelihood for signal + background model: {model.logpdf(pars=sigPlusBkg_pars, data=observations)}")
+        print(f"Log likelihood for background only model: {model.logpdf(pars=bkg_pars, data=observations)}")
+
+    return model.logpdf(pars=sigPlusBkg_pars, data=observations), model.logpdf(pars=bkg_pars, data=observations)
 
 def debug_print(model):
     print(f"  channels: {model.config.channels}")
@@ -31,19 +44,29 @@ def debug_print(model):
 
 def main():
     parser = argparse.ArgumentParser(description="Perform a single-channel statistical fit with uncorrelated background")
-    parser.add_argument("--n_signal", type=float, default=1.0)
-    parser.add_argument("--n_background", type=float, default=1.0)
-    parser.add_argument("--background_uncertainty", type=float, default=1.0)
-    parser.add_argument("--do_debug_print", type=bool, default=True)
+    parser.add_argument("--n_signal", type=float, default=1.0, help="Number of signal events expected in the signal region")
+    parser.add_argument("--n_background", type=float, default=1.0, help="Number of background events expected in the signal region")
+    parser.add_argument("--background_uncertainty", type=float, default=1.0, help="Uncertainty on number of background events expected in the signal region. For example, if the number of background events is 50 +- 5, you would pass 5 into this argument.")
+    parser.add_argument("--n_observed", type=int, default=1, help="Number of events observed in data in the signal region")
+    parser.add_argument("--do_debug_print", type=bool, default=False)
     args = parser.parse_args()
+    global do_debug_print
+    do_debug_print = args.do_debug_print
 
-    model = build_model(args.n_signal, args.n_background, args.background_uncertainty, args.do_debug_print)
+    model = build_model(args.n_signal, args.n_background, args.background_uncertainty)
 
     # For a single channel, the parameters are [mu, gamma_b] (see https://pyhf.github.io/pyhf-tutorial/helloworld/)
     # Mu is the signal strength, gamma_b is a multiplicative factor calculated by the background uncertainty given an uncorrelated background
     # Here by default the model automatically calculates what mu and gamma_b should be
     init_pars = model.config.suggested_init()
     model.expected_actualdata(init_pars)
+
+    # Check the background only model
+    bkg_pars = background_only_parameters(model, init_pars)
+    model.expected_actualdata(bkg_pars)
+
+    # Compute the loglikelihood
+    compute_log_likelihood(model, args.n_observed, init_pars, bkg_pars)
 
 if __name__ == "__main__":
     main()
